d612c853fce34ad3560672135c4f3b34830c974f
Naveen Kumar <karanamunaveen2002@gmail.com>
2024-03-31 17:48:11 +0530

    Build: Profile Services

---
+++ src/main/java/rest_jax/messenger/resources/ProfileResources.java
@@ -0,0 +1,25 @@
+package rest_jax.messenger.resources;
+
+import java.util.List;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import rest_jax.messenger.model.Profile;
+import rest_jax.messenger.service.ProfileService;
+
+@Path("/profiles")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ProfileResources {
+
+	private ProfileService profileService = new ProfileService();
+	
+	@GET
+	public List<Profile> getProfiles(){
+		return profileService.getAllProfiles();
+	}
+	
+}
