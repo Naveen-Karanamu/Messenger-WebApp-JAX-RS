d10645c805efc6ae84543ed586cc99b1578b61f4
Naveen Kumar <karanamunaveen2002@gmail.com>
2024-03-31 19:08:35 +0530

    Built: GET one profile & UPDATE API

--- src/main/java/rest_jax/messenger/resources/ProfileResources.java
+++ src/main/java/rest_jax/messenger/resources/ProfileResources.java
@@ -5,7 +5,9 @@ import java.util.List;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import rest_jax.messenger.model.Profile;
@@ -23,10 +25,23 @@ public class ProfileResources {
 		return profileService.getAllProfiles();
 	}
 	
+	@GET
+	@Path("/{profileName}")
+	public Profile getOneProfile(@PathParam("profileName") String profileName) {
+		return profileService.getProfile(profileName);
+	}
+	
 	@POST
 	@Path("/add")
 	public Profile addProfile(Profile profile) {
 		return profileService.addProfile(profile);
+	}
+	
+	@PUT
+	@Path("/update/{profileName}")
+	public Profile updateProfile(@PathParam("profileName") String profileName, Profile profile) {
+		profile.setProfileName(profileName);
+		return profileService.updateProfile(profile);
 	}
 	
 }
