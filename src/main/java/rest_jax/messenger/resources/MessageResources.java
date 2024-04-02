558fea3e4f0e7732b341b36a20c627b686076a0a
Naveen Kumar <karanamunaveen2002@gmail.com>
2024-04-02 11:24:31 +0530

    Handled Exceptions: MessageResource

--- src/main/java/rest_jax/messenger/resources/MessageResources.java
+++ src/main/java/rest_jax/messenger/resources/MessageResources.java
@@ -1,3 +1,75 @@
+//package rest_jax.messenger.resources;
+//
+//import java.util.List;
+//
+//import jakarta.ws.rs.BeanParam;
+//import jakarta.ws.rs.Consumes;
+//import jakarta.ws.rs.DELETE;
+//import jakarta.ws.rs.GET;
+//import jakarta.ws.rs.POST;
+//import jakarta.ws.rs.PUT;
+//import jakarta.ws.rs.Path;
+//import jakarta.ws.rs.PathParam;
+//import jakarta.ws.rs.Produces;
+//import jakarta.ws.rs.core.MediaType;
+//import rest_jax.messenger.model.Message;
+//import rest_jax.messenger.service.MessageService;
+//
+//@Path("/messages")
+//public class MessageResources {
+//	
+//	MessageService messageService = new MessageService();
+//
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean) {
+//		if(filterBean.getYear() > 0) {
+//			return messageService.getAllMessagesForYear(filterBean.getYear());
+//		}
+//		if (filterBean.getStart()>=0 && filterBean.getSize() >= 0) {
+//			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+//		}
+//		return messageService.getAllMessages();
+//	}
+//	
+//	@GET
+//	@Path("/{messageId}")
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Message getOneMessage(@PathParam("messageId") long messageId){		
+//		return messageService.getMessage(messageId);
+//	}
+//	
+//	@POST
+//	@Path("/add")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Message addMessage(Message message) {
+//		return messageService.addMessage(message);
+//	} 
+//	
+//	@PUT
+//	@Path("/update/{messageId}")
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+//		message.setId(messageId);
+//;		return messageService.updateMessage(message);
+//	}
+//	
+//	@DELETE
+//	@Path("/delete/{messageId}")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Message deleteMessage(@PathParam("messageId") long messageId) {
+//		return messageService.removeMessage(messageId);
+//	}
+//	
+//	@Path("/{messageId}/comments") // This maps to CommentResource
+//    public CommentResource forwardToCommentResource(@PathParam("messageId") long messageId) {
+//        return new CommentResource();
+//    }
+//	
+//}
+
 package rest_jax.messenger.resources;
 
 import java.util.List;
@@ -11,78 +83,117 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
-import rest_jax.messenger.exception.ErrorMessage;
 import rest_jax.messenger.model.Message;
 import rest_jax.messenger.service.MessageService;
 
 @Path("/messages")
 public class MessageResources {
-	
-	MessageService messageService = new MessageService();
+    
+    private final MessageService messageService = new MessageService();
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean) {
-		if(filterBean.getYear() > 0) {
-			return messageService.getAllMessagesForYear(filterBean.getYear());
-		}
-		if (filterBean.getStart()>=0 && filterBean.getSize() >= 0) {
-			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
-		}
-		return messageService.getAllMessages();
-	}
-	
-	@GET
-	@Path("/{messageId}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message getOneMessage(@PathParam("messageId") long messageId){
-		ErrorMessage errorMessage = new ErrorMessage("Not Found", 404, "www.google.com");
-		Response response = Response.status(Status.NOT_FOUND)
-				.entity(errorMessage)
-				.build();
-		
-		if(messageId == 0) {
-			throw new WebApplicationException(response);
-		}
-		
-		return messageService.getMessage(messageId);
-	}
-	
-	@POST
-	@Path("/add")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response addMessage(Message message) {
-//		return messageService.addMessage(message);
-		Message newMessage= messageService.addMessage(message);
-		return Response.status(Status.CREATED)
-				.entity(newMessage)
-				.build();
-	} 
-	
-	@PUT
-	@Path("/update/{messageId}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
-		message.setId(messageId);
-;		return messageService.updateMessage(message);
-	}
-	
-	@DELETE
-	@Path("/delete/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message deleteMessage(@PathParam("messageId") long messageId) {
-		return messageService.removeMessage(messageId);
-	}
-	
-	@Path("/{messageId}/comments") // This maps to CommentResource
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getMessages(@BeanParam MessageFilterBean filterBean) {
+        try {
+            if (filterBean.getYear() > 0) {
+                List<Message> messages = messageService.getAllMessagesForYear(filterBean.getYear());
+                return Response.ok().entity(messages).build();
+            } else if (filterBean.getStart() >= 0 && filterBean.getSize() >= 0) {
+                List<Message> messages = messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+                return Response.ok().entity(messages).build();
+            } else {
+                List<Message> messages = messageService.getAllMessages();
+                return Response.ok().entity(messages).build();
+            }
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                           .entity("Error occurred while fetching messages: " + e.getMessage())
+                           .build();
+        }
+    }
+    
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getOneMessage(@PathParam("messageId") long messageId){
+        try {
+            Message message = messageService.getMessage(messageId);
+            if (message != null) {
+                return Response.ok().entity(message).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND)
+                               .entity("Message not found with id: " + messageId)
+                               .build();
+            }
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                           .entity("Error occurred while fetching the message: " + e.getMessage())
+                           .build();
+        }
+    }
+    
+    @POST
+    @Path("/add")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addMessage(Message message) {
+        try {
+            Message addedMessage = messageService.addMessage(message);
+            return Response.status(Response.Status.CREATED).entity(addedMessage).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                           .entity("Error occurred while adding message: " + e.getMessage())
+                           .build();
+        }
+    } 
+    
+    @PUT
+    @Path("/update/{messageId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateMessage(@PathParam("messageId") long messageId, Message message) {
+        try {
+            Message existingMessage = messageService.getMessage(messageId);
+            if (existingMessage == null) {
+                return Response.status(Response.Status.NOT_FOUND)
+                               .entity("Message not found with id: " + messageId)
+                               .build();
+            }
+            message.setId(messageId);
+            Message updatedMessage = messageService.updateMessage(message);
+            return Response.ok().entity(updatedMessage).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                           .entity("Error occurred while updating message: " + e.getMessage())
+                           .build();
+        }
+    }
+
+    
+    @DELETE
+    @Path("/delete/{messageId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteMessage(@PathParam("messageId") long messageId) {
+        try {
+            Message deletedMessage = messageService.removeMessage(messageId);
+            if (deletedMessage != null) {
+                return Response.ok().entity(deletedMessage).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND)
+                               .entity("Message not found with id: " + messageId)
+                               .build();
+            }
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                           .entity("Error occurred while deleting message: " + e.getMessage())
+                           .build();
+        }
+    }
+    
+    @Path("/{messageId}/comments") // This maps to CommentResource
     public CommentResource forwardToCommentResource(@PathParam("messageId") long messageId) {
         return new CommentResource();
     }
-	
 }
